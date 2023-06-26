29088763bbc59f880aa8f5f60ea72ac122fb41d5
ChuYeonBin <cndusqls98@gmail.com>
2023-06-26 15:29:25 +0900

    fix: DefaultLayout component props 수정

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from 'react';
 import styled from 'styled-components';
 
 interface DefaultLayoutProps {
-  children: JSX.Element;
+  children?: ReactNode;
 }
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {

a31846c0f8d796b96a4442adacf47adedcd35e69
ChuYeonBin <cndusqls98@gmail.com>
2023-06-26 13:45:54 +0900

    feat: monthOf5YearFormat util 함수 구현

--- src/utils/calendar.ts
+++ src/utils/calendar.ts
@@ -1,12 +1,28 @@
 import { getYear, getMonth } from 'date-fns';
 
-export function monthOfYearFormat(dates: Date[] | number[]) {
-  const dateFormats = dates.map((date) => {
-    const year = getYear(date);
-    const month = getMonth(date);
+export function monthOf5YearFormat() {
+  const date = new Date();
+  const currentYear = getYear(date);
+  const currentMonth = getMonth(date) + 1;
+  const currentDates = [];
+  const dates = [];
 
-    return `${year}년 ${month}월`;
-  });
+  for (let i = currentMonth; i >= 1; i--) {
+    const dateFormat = `${currentYear}년 ${i}월`;
+    currentDates.push(dateFormat);
+  }
+  dates.push(...currentDates);
 
-  return dateFormats;
+  for (let i = currentYear - 1; i >= currentYear - 5; i--) {
+    for (let j = 12; j >= 1; j--) {
+      const dateFormat = `${i}년 ${j}월`;
+      dates.push(dateFormat);
+    }
+  }
+
+  for (let i = currentMonth - 1; i >= 1; i--) {
+    dates.pop();
+  }
+
+  return dates;
 }

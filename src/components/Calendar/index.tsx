91246fa292887f62c931c07d1da21d5f9786cf77
ChuYeonBin <cndusqls98@gmail.com>
2023-06-28 17:05:29 +0900

    feat: CalendarBody component UI 구현

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 import CalendarHeader from './CalendarHeader';
+import CalendarBody from './CalendarBody';
 
 export default function Calendar() {
   return (
     <St.Container>
       <CalendarHeader />
+      <CalendarBody />
     </St.Container>
   );
 }

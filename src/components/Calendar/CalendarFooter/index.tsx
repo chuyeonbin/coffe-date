944d7d03c1ed072949be647ed4a33266b879bec9
ChuYeonBin <cndusqls98@gmail.com>
2023-07-04 13:58:30 +0900

    feat: CalendarFooter component UI 구현

---
+++ src/components/Calendar/CalendarFooter/index.tsx
@@ -0,0 +1,10 @@
+import CoffeeDetails from './CoffeeDetails';
+import CalendarLayout from '../../layouts/CalendarLayout';
+
+export default function CalendarFooter() {
+  return (
+    <CalendarLayout>
+      <CoffeeDetails />
+    </CalendarLayout>
+  );
+}

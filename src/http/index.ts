6c444e17e90305f7623d21c116040ff3e75b6b83
ChuYeonBin <cndusqls98@gmail.com>
2023-07-06 15:13:10 +0900

    feat: http 통신 모듈 구현

---
+++ src/http/index.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+const BASE_URL = process.env.NODE_ENV == 'development' ? 'http://localhost:8080' : '';
+
+class HttpClient {
+  private httpClient: AxiosInstance;
+
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: BASE_URL,
+    });
+  }
+
+  async get<T = any>(url: string) {
+    const response: AxiosResponse<T> = await this.httpClient.post(url);
+
+    return response.data;
+  }
+
+  async post<T = any>(url: string, data: Object) {
+    const response: AxiosResponse<T> = await this.httpClient.post(url, data);
+
+    return response.data;
+  }
+}
+
+const httpClient = new HttpClient();
+
+export default httpClient;

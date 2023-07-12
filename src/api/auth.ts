2c17eec8598a95402983f5425eed73126389f1a5
ChuYeonBin <cndusqls98@gmail.com>
2023-07-12 14:47:28 +0900

    feat: checkCodeAPI 함수 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,3 +3,7 @@ import httpClient from '../http';
 export async function sendEmailAPI(payload: { email: string }) {
   return httpClient.post('/auth/sendemail', payload);
 }
+
+export default function checkCodeAPI(code: string) {
+  return httpClient.get(`/auth/code/${code}`);
+}

8489eb5c6061500f489fd6cc9b2d7c5f08979477
ChuYeonBin <cndusqls98@gmail.com>
2023-07-13 23:36:05 +0900

    feat: code 유효한지 여부에 따라 라우팅

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,12 +1,30 @@
 import { styled } from 'styled-components';
 import RegisterModal from '../../components/RegisterModal';
+import { useEffect } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Navigate, useNavigate, useSearchParams } from 'react-router-dom';
+import checkCodeAPI from '../../api/auth';
 
 export default function RegisterPage() {
-  return (
-    <St.Container>
-      <RegisterModal />
-    </St.Container>
-  );
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const code = searchParams.get('code');
+
+  const { data, isLoading } = useQuery(['resgiter-code'], () => checkCodeAPI(code), {
+    enabled: !!code,
+  });
+
+  useEffect(() => {
+    if (data) {
+      if (!data.checked) {
+        navigate('/login');
+      }
+    }
+  }, [data, isLoading]);
+
+  if (!code) return <Navigate to='/login' />;
+
+  return <St.Container>{!isLoading && <RegisterModal />}</St.Container>;
 }
 
 const St = {

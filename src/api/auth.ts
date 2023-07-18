a30a4245b4b707db84ca12829c2d0a5a6fbdd27a
ChuYeonBin <cndusqls98@gmail.com>
2023-07-18 13:42:38 +0900

    feat: 회원가입 api 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,13 +1,23 @@
-import httpClient from '../http';
+import { httpClient } from '../http';
 
 export function sendEmailAPI(payload: { email: string }) {
   return httpClient.post('/auth/sendemail', payload);
 }
 
 export function checkCodeAPI(code: string | null) {
-  return httpClient.get<{ checked: boolean }>(`/auth/code/${code}`);
+  return httpClient.get<{ checked: boolean; email: string }>(`/auth/code/${code}`);
 }
 
 export function duplicationCheckAPI(payload: { nickname: string }) {
   return httpClient.post<{ checked: boolean }>('/auth/duplicate', payload);
+}
+
+export function signUpAPI(payload: { email: string; nickname: string }) {
+  return httpClient.post<{
+    user: { nickname: string; email: string };
+    tokens: {
+      access_token: string;
+      refresh_token: string;
+    };
+  }>('auth/register', payload);
 }

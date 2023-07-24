c63533996b3b0c3c9d727c6064961871dd8821b1
ChuYeonBin <cndusqls98@gmail.com>
2023-07-24 15:07:30 +0900

    feat: Loading component 구현

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/DefaultRoter';
 import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Loading from './components/Loading';
 
 const queryClient = new QueryClient();
 
@@ -14,7 +15,9 @@ root.render(
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={true} />
-        <RouterProvider router={router} />
+        <Suspense fallback={<Loading />}>
+          <RouterProvider router={router} />
+        </Suspense>
       </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,

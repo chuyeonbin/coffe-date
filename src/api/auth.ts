d93df6814ad1b314859be7170eec29b6ce39a369
ChuYeonBin <cndusqls98@gmail.com>
2023-07-27 19:21:01 +0900

    feat: 이메일 로그인 api 구현

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -8,6 +8,13 @@ export function checkCodeAPI(code: string | null) {
   return httpClient.get<{ checked: boolean; email: string }>(`/auth/code/${code}`);
 }
 
+export function emailLoginAPI(code: string | null) {
+  return httpClient.get<{
+    user: { nickname: string; email: string };
+    access_token: string;
+  }>(`auth/code/${code}`);
+}
+
 export function duplicationCheckAPI(payload: { nickname: string }) {
   return httpClient.post<{ checked: boolean }>('/auth/duplicate', payload);
 }

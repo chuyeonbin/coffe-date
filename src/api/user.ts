487129cee4137da0d857ee5ef03ad7931b7b6bec
ChuYeonBin <cndusqls98@gmail.com>
2023-08-03 18:03:20 +0900

    chore: 불필요한 코드 제거

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,5 @@
 import { authHttpClient } from '../http';
 
 export function getUserAPI() {
-  console.log('test');
   return authHttpClient.get<{ email: string; nickname: string } | null>('/user');
 }

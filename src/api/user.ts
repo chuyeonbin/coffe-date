db889318bc2c90cf25dfefba830dcf47b1d646b4
ChuYeonBin <cndusqls98@gmail.com>
2023-08-03 18:59:49 +0900

    feat: logout api 구현

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,3 +3,7 @@ import { authHttpClient } from '../http';
 export function getUserAPI() {
   return authHttpClient.get<{ email: string; nickname: string } | null>('/user');
 }
+
+export function logOutAPI() {
+  return authHttpClient.post('/user/logout');
+}

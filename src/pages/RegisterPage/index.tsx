a01b35c2a2faf1cb429308091f7686a670b0a186
ChuYeonBin <cndusqls98@gmail.com>
2023-08-04 16:44:20 +0900

    fix: register page 등록 여부 확인 후 로그인 페이지로 가는 버그 수정

--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,6 +1,5 @@
 import { styled } from 'styled-components';
 import RegisterModal from '../../components/RegisterModal';
-import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { Navigate, useNavigate, useSearchParams } from 'react-router-dom';
 import { checkCodeAPI } from '../../api/auth';
@@ -8,17 +7,10 @@ import Loading from '../../components/Loading';
 
 export default function RegisterPage() {
   const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
   const code = searchParams.get('code');
   const { data, isLoading, isError } = useQuery(['resgiter-code'], () => checkCodeAPI(code), {
     enabled: !!code,
   });
-
-  useEffect(() => {
-    if (!data?.email) {
-      navigate('/login');
-    }
-  }, [data, isLoading, isError]);
 
   if (!code || isError) return <Navigate to='/login' />;
 

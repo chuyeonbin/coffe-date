b915d02d5d362e58013b899d40918880f51c53d0
ChuYeonBin <cndusqls98@gmail.com>
2023-08-24 18:57:07 +0900

    feat: logs 데이터 요청후 받아와서 전역상태로 저장

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
 import DefaultLayout from '../../components/layouts/DefaultLayout';
 import Calendar from '../../components/Calendar';
+import { useQuery } from '@tanstack/react-query';
+import { getLogsAPI } from '../../api/log';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { currentDateState } from '../../store/date';
+import { format } from 'date-fns';
+import { logsState } from '../../store/logs';
 
 export default function MainPage() {
+  const currnentDate = useRecoilValue(currentDateState);
+  const setLogs = useSetRecoilState(logsState);
+  const currnentDateFormmat = format(currnentDate, 'yyyy-MM-dd');
+  const { data, refetch } = useQuery(['logs'], () => getLogsAPI(currnentDateFormmat));
+
+  useEffect(() => {
+    refetch();
+  }, [currnentDate]);
+
+  useEffect(() => {
+    if (data) {
+      setLogs(data.logs);
+    }
+  }, [data]);
+
   return (
     <DefaultLayout>
       <Calendar />

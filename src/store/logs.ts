7cb140bc14e76dff081b72b7d199c3a1648e26db
ChuYeonBin <cndusqls98@gmail.com>
2023-08-25 17:26:06 +0900

    feat: groupedByDateLogsState selector 구현

--- src/store/logs.ts
+++ src/store/logs.ts
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export interface LogType {
   id: number;
@@ -13,4 +13,23 @@ export interface LogType {
 export const logsState = atom<LogType[]>({
   key: 'logs',
   default: [],
+});
+
+export const groupedByDateLogsState = selector({
+  key: 'groupedByDateLogs',
+  get: ({ get }) => {
+    const logs = get(logsState);
+
+    const groupedByDateLogs: { [key: string]: LogType[] } = {};
+
+    logs.forEach((log) => {
+      const slicedDate = log.date.slice(0, 10);
+      if (!groupedByDateLogs[slicedDate]) {
+        groupedByDateLogs[slicedDate] = [];
+      }
+      groupedByDateLogs[slicedDate].push(log);
+    });
+
+    return groupedByDateLogs;
+  },
 });

7dfbb7c381e674280d42f184cd12467669cb6412
ChuYeonBin <cndusqls98@gmail.com>
2023-08-30 15:15:16 +0900

    style: CoffeeDetail component style 수정

--- src/components/Calendar/CalendarFooter/CoffeeDetail/index.tsx
+++ src/components/Calendar/CalendarFooter/CoffeeDetail/index.tsx
@@ -1,38 +1,50 @@
 import styled from 'styled-components';
 import { LogType } from '../../../../store/logs';
+import { useNavigate } from 'react-router-dom';
 
 interface CoffeeDetailProps {
   log: LogType;
 }
 
 export default function CoffeeDetail({ log }: CoffeeDetailProps) {
+  const navigate = useNavigate();
+
+  const goToEditPage = () => {
+    navigate(`log-edit/${log.id}`);
+  };
+
   return (
     <St.Container>
-      <St.Log>
-        <St.CoffeeImageContainer>
-          <St.CoffeeImage src={process.env.PUBLIC_URL + '/coffee_detail.svg'} alt='coffee_image' />
-        </St.CoffeeImageContainer>
-        <St.CoffeeContainer>
-          <St.Price>{log.price.toLocaleString()}원</St.Price>
-          <St.CafeInfo>
-            {log.cafe} | {log.coffee}
-          </St.CafeInfo>
-        </St.CoffeeContainer>
-      </St.Log>
+      <St.CoffeeImageContainer>
+        <St.CoffeeImage src={process.env.PUBLIC_URL + '/coffee_detail.svg'} alt='coffee_image' />
+      </St.CoffeeImageContainer>
+      <St.CoffeeContainer>
+        <St.Price>{log.price.toLocaleString()}원</St.Price>
+        <St.CafeInfo>
+          {log.cafe} | {log.coffee}
+        </St.CafeInfo>
+      </St.CoffeeContainer>
+      <St.ButtonContainer>
+        <EditButton onClick={goToEditPage}>수정</EditButton>
+        <pre> | </pre>
+        <RemoveButton>삭제</RemoveButton>
+      </St.ButtonContainer>
     </St.Container>
   );
 }
 
 const St = {
-  Container: styled.div``,
-
-  Log: styled.div`
+  Container: styled.div`
     margin-bottom: 1rem;
     padding: 1rem;
     display: flex;
     align-items: center;
     background-color: ${({ theme }) => theme.colors.bgElement5};
     border-radius: 8px;
+
+    &:hover {
+      transform: scale(1.02);
+    }
   `,
 
   CoffeeImageContainer: styled.div`
@@ -64,4 +76,26 @@ const St = {
   CafeInfo: styled.p`
     font-size: ${({ theme }) => theme.fontSizes.sm};
   `,
+
+  ButtonContainer: styled.div`
+    display: flex;
+    color: ${({ theme }) => theme.colors.text1};
+  `,
+
+  Button: styled.button`
+    font-size: ${({ theme }) => theme.fontSizes.sm};
+    color: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  `,
 };
+
+const EditButton = styled(St.Button)``;
+
+const RemoveButton = styled(St.Button)`
+  &:hover {
+    color: red;
+  }
+`;
